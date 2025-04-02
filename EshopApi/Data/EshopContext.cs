41554d518bedb88321c6adaad28420d2c31e3c0e
Igor Minenko <igorymin2002@gmail.com>
2025-04-02 11:18:57 +0200

    feat: Prepare initial database seed with sample products

--- EshopApi/Data/EshopContext.cs
+++ EshopApi/Data/EshopContext.cs
@@ -3,9 +3,17 @@ using EshopApi.Entities;
 
 namespace EshopApi.Data
 {
-    public class EshopContext(DbContextOptions<EshopContext> options)  
-        : DbContext(options)
+    public class EshopContext(DbContextOptions<EshopContext> options)  : DbContext(options)
     {
         public DbSet<Product> Products => Set<Product>();   
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "iPhone 15", ImageUrl = "someUrl1", Price = 999.99m, Description = "Apple smartphone" },
+                new Product { Id = 2, Name = "Samsung Galaxy S24", ImageUrl = "someUrl3", Price = 899.99m, Description = "Samsung smartphone" },
+                new Product { Id = 3, Name = "Google Pixel 8", Price = 799.99m, ImageUrl = "someUrl3", Description = "Google smartphone" }
+            );
+        }
     }
 }
---
+++ EshopApi/Data/Migrations/20250402091624_SeedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EshopApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Name", "Price", "Stock" },
+                values: new object[,]
+                {
+                    { 1, "Apple smartphone", "someUrl1", "iPhone 15", 999.99m, 0 },
+                    { 2, "Samsung smartphone", "someUrl3", "Samsung Galaxy S24", 899.99m, 0 },
+                    { 3, "Google smartphone", "someUrl3", "Google Pixel 8", 799.99m, 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
